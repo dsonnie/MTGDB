24139421c81afa37dd64afe8fa1d334ff8f00cec
Dave Sonnie <dsonnie@gmail.com>
2026-02-20 17:34:52 -0500

    Update staging.py: Automated staging table creation and initial load of CSV

--- 01_staging_prep.py
+++ 01_staging_prep.py
@@ -10,9 +10,15 @@ Copyright (c) 2026 Dave Sonnie
 # This script does not run the command, only produces the necessary text to do so; 
 # Use or adjust as necessary based on the specific implementation
 import csv
+import os
+import psycopg
 
+working_dir = 'Postgres' + os.sep + 'Postgres-MTG'
 csv_file = 'cards.csv'
 table_name = 'staging_cards'
+
+# Docker Postgres
+DATABASE_URL = "postgresql://mtg_admin@localhost:5432/mtg_db"
 
 with open(csv_file, newline="", encoding="utf-8") as f: 
     reader = csv.reader(f)
@@ -22,8 +28,34 @@ columns = ',\n'.join(
     f'"{h.strip()}" TEXT' for h in headers
 )
 
-create_sql = f'CREATE TABLE {table_name} ({columns});';
-print(create_sql)
+# Build query
+create_sql = f"""
+CREATE TABLE IF NOT EXISTS {table_name} (
+{columns}
+);
+"""
+
+#create_sql = f'CREATE TABLE IF NOT EXISTS {table_name} ({columns});';
+#print(create_sql)
+
+
+# ---------- EXECUTE ----------
+with psycopg.connect(DATABASE_URL) as conn:
+    with conn.cursor() as cur:
+
+        # Cleanup old staging table if it exists
+        cur.execute(f'DROP TABLE IF EXISTS {table_name} CASCADE;')
+        # Create table
+        cur.execute(create_sql)
+
+        # Bulk load CSV
+        with open(csv_file, "r", encoding="utf-8") as f:
+            with cur.copy(f"COPY {table_name} FROM STDIN WITH CSV HEADER") as copy:
+                copy.write(f.read())
+
+    conn.commit()
+
+print("Table created and data loaded successfully.")
 
 # Psql command to copy cards to the staging table
 # \copy staging_cards FROM 'cards.csv' WITH (FORMAT csv, HEADER true);
